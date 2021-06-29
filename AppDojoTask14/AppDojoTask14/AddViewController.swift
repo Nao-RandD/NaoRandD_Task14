098b86ee248b27538fc18783310dc90a332c5819
Nao-RandD <nao.r.and.d@gmail.com>
2021-06-30 08:03:38 +0900

    TableViewに項目を追加する機能を追加

---
+++ AppDojoTask14/AppDojoTask14/AddViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AddViewController.swift
+//  AppDojoTask14
+//
+//  Created by Naoyuki Kan on 2021/06/29.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+    @IBOutlet weak var addTextField: UITextField!
+}
--- AppDojoTask14/AppDojoTask14/ViewController.swift
+++ AppDojoTask14/AppDojoTask14/ViewController.swift
@@ -15,7 +15,8 @@ struct CheckItem {
 class ViewController: UIViewController {
 
     @IBOutlet private weak var tableView: UITableView!
-    private let tableItems: [CheckItem] = [
+
+    private var tableItems: [CheckItem] = [
         CheckItem(name: "りんご", isChecked: false),
         CheckItem(name: "みかん", isChecked: true),
         CheckItem(name: "バナナ", isChecked: false),
@@ -28,6 +29,16 @@ class ViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
     }
+
+    @IBAction func tapCancel(segue: UIStoryboardSegue) {}
+
+    @IBAction func tapSave(segue: UIStoryboardSegue) {
+        if let vc = segue.source as? AddViewController {
+            let item = CheckItem(name: vc.addTextField.text!, isChecked: false)
+            tableItems.append(item)
+            tableView.reloadData()
+        }
+    }
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
@@ -38,9 +49,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell  = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CheckItemCell
-
         cell.configure(checkItem: tableItems[indexPath.row])
-
         return cell
     }
 }
